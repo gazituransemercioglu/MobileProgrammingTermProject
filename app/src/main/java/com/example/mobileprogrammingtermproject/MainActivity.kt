d7a6b6e6fa92e1aa7698a159eaf754007192d448
gazituransemercioglu <102522962+gazituransemercioglu@users.noreply.github.com>
2022-04-09 23:22:40 +0300

    Main Interface has been Created(draft version).

--- app/src/main/java/com/example/mobileprogrammingtermproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingtermproject/MainActivity.kt
@@ -2,6 +2,14 @@ package com.example.mobileprogrammingtermproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.widget.Toast
+
+
 
 
 class MainActivity : AppCompatActivity() {
