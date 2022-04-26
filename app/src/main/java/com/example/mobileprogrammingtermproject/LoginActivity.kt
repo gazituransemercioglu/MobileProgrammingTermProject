f6ea37d69148a426e288759f297cfd05db639c43
ismailsengul <ismailsengul64@gmail.com>
2022-04-26 05:30:54 +0300

    Login activity implemented.

---
+++ app/src/main/java/com/example/mobileprogrammingtermproject/LoginActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mobileprogrammingtermproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+    fun login(view : View){
+
+    }
+    fun signUp(view: View){
+
+    }
+}
--- app/src/main/java/com/example/mobileprogrammingtermproject/MainActivity.kt
+++ app/src/main/java/com/example/mobileprogrammingtermproject/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.mobileprogrammingtermproject
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.content.Context
+import android.content.Intent
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import android.view.View
 import android.widget.Toast
 
 
@@ -16,6 +18,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun login(view : View){
+        val intent = Intent(applicationContext,LoginActivity :: class.java)
+        startActivity(intent)
     }
 }
 
